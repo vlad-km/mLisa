0ddc2387d5904a1e94dbd20bd547191475b51bfa
vlad <26150554+vlad-km@users.noreply.github.com>
2021-05-08 09:37:17 +0300

    Create 00-prelude.lisp
    Packages for LISA under JSCL compilation

---
+++ 00-prelude.lisp
@@ -0,0 +1,21 @@
+;;; -*- mode:lisp; coding:utf-8 -*-
+
+;;; Modification for JSCL under Electron platform
+;;; Copyright (C) 2021 Vladimir K. Mezentsev (@vlad-km)
+
+
+(eval-when (:compile-toplevel :load-toplevel :execute)
+  (unless (find-package :lisa)
+    (make-package :lisa :use (list 'cl)))
+  (unless (find-package :lisa-user)
+    (make-package :lisa-user :use (list 'cl)))
+  (unless (find-package :reflect)
+    (make-package :reflect :use (list 'cl)))
+  (unless (find-package :belief)
+    (make-package :belief :use (list 'cl)))
+  (unless (find-package :heap)
+    (make-package :heap :use (list 'cl))))
+
+
+
+;;; EOF
